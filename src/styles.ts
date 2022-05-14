3761b4f5beef3d50673878fb20fc4c00a867e2e3
Fabricio <fabriciopolicarpo0@gmail.com>
2022-05-13 23:30:57 -0400

    Initial commit

---
+++ src/styles.ts
@@ -0,0 +1,18 @@
+
+import styled from "styled-components";
+
+
+export const GridWrapper = styled.div`
+  display: grid;
+`
+
+export const Wrapper = styled.div`
+  max-width: 300px;
+  width: 100%;
+  height: 200px;
+  background: #ffffff;
+  box-shadow: 0px 30px 60px rgba(38, 46, 59, 0.3);
+  border-radius: 20px;
+`;
+
+export const Title = styled.h1``;
